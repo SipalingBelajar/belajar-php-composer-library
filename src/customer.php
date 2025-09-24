1efa6524cc7c9df30203d39c09fa55c31d9dfb79
Your Name <you@example.com>
2025-09-24 11:33:41 +0700

    contoh sederhana library composer

---
+++ src/customer.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Armanphp\Belajar;
+
+    class Customer
+    {
+        public function __construct()
+        {
+            echo "Hello Customer" . PHP_EOL;
+        }
+
+        public function sayHello(string $name)
+        {
+           return "Hello $name, My Name is Customer" . PHP_EOL;
+        }
+    }
